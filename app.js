f4480a589adf53e0f3983e1642e990a76074a1fa
syedubaidshah <obaidshah0099@gmail.com>
2024-06-25 01:35:43 +0500

    hover

---
+++ app.js
@@ -0,0 +1,15 @@
+function change1() {
+  document.getElementById("img1").src = "pic2.jpg";
+}
+
+function change2() {
+  document.getElementById("img1").src = "pic1.jpg";
+}
+
+function change3() {
+  document.getElementById("img2").src = "pic3.jpg";
+}
+
+function change4() {
+  document.getElementById("img2").src = "pic4.jpg";
+}
